74e2dd8a5d0b529558d0a922436e35cc5152bcff
Mandar Joshi <mandar.joshi@bluepineapple.io>
2022-12-27 12:52:22 +0530

    code optimized

--- src/login.js
+++ src/login.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import fire from "./config/fire";
 import "./login.css";
 
+// code optimized
+
 class Login extends Component {
   constructor(props) {
     super(props);
