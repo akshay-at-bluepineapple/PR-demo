788d447f3fe5655c4f9311c285db824e3d23e4fd
sayali-more-bluepineapple <112163550+sayali-more-bluepineapple@users.noreply.github.com>
2022-12-28 16:38:31 +0530

    Added home

---
+++ src/home.js
@@ -0,0 +1,22 @@
+/* import React, { Component } from 'react';
+import fire from './config/fire'
+import Header from './header/header'
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {  }
+    }
+
+   
+    render() { 
+        return ( <div>
+            <Header/>
+            <h3>You are home</h3>
+        </div> );
+    }
+}
+ 
+export default Home; 
+ */
