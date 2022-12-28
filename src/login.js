f74af4d4ec669c87a477002630f8bdd9f211e3f7
Mandar Joshi <mandar.joshi@bluepineapple.io>
2022-12-28 12:03:59 +0530

    removed repeated function

--- src/login.js
+++ src/login.js
@@ -37,25 +37,10 @@ class Login extends Component {
       });
   }
 
-  signup(e) {
-    e.preventDefault();
-    fire
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .catch(error => {
-        console.log(error);
-      });
-  }
 
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
-
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-
 
   render() {
     return (
